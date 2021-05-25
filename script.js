b9ea1bcb9d8dfb9aacb0f1ceed7a4d30e1b7fe2e
aherrera05 <aman_hr@hotmail.com>
2021-05-24 21:32:59 -0600

    Listo

---
+++ script.js
@@ -0,0 +1,69 @@
+let randomNumber = Math.floor(Math.random() * 100) + 1;
+const guesses = document.querySelector('.guesses');
+const lastResult = document.querySelector('.lastResult');
+const lowOrHi = document.querySelector('.lowOrhi');
+
+const guessSubmit = document.querySelector('.guessSubmit');
+const guessField = document.querySelector('.guessField');
+
+let guessCount = 1;
+let resetButtom;
+
+function checkGuess(){
+    let userGuess = Number(guessField.value);
+    
+    if(userGuess === randomNumber){
+    lastResult.textContent = 'Congratulations! You got it right!';
+    lastResult.getElementsByClassName.backgroundColor = 'green';
+    lowOrHi.textContent = '';
+    setGameOver();
+    
+    } else if (guessCount === 10){
+        lastResult.textContent = 'Game Over!!!';
+      } else {
+          lastResult.textContent = 'Wrong!';
+          lastResult.style.backgroundColor = 'red';
+          if (userGuess < randomNumber){
+              lowOrHi.textContent = 'Too Low';
+          } else {
+              lowOrHi.textContent = 'Too high';
+          }
+        }
+    
+
+      guessCount ++;
+      guessField.value = '';
+      guessField.focus();
+    
+}
+
+guessSubmit.addEventListener('click',checkGuess);
+
+function setGameOver(){
+    guessField.disabled = true;
+    guessSubmit.disabled = true;
+    resetButtom = document.createElement('button');
+    resetButtom.textContent = 'Start new game';
+    document.body.append(resetButtom);
+    resetButtom.addEventListener('click', resetGame);
+
+}
+
+function resetGame(){
+    guessCount = 1;
+    
+    const resetParas = document.querySelectorAll('.resultParas p');
+    for(let i = 0; i < resetParas.length; i++){
+     resetParas[i].textContent = '';
+    }
+    resetButtom.parentNode.removeChild(resetButtom);
+
+    guessField.disabled = false;
+    guessSubmit.disabled = false;
+    guessField.value = '';
+    guessField.focus();
+
+    lastResult.style.backgroundColor = 'white';
+
+    randomNumber = Math.floor(Math.random() * 100) + 1;
+}
